bc05d617a250ae1d75b20774219d0a90b3a3839d
nolangh <nolanghiggins@gmail.com>
2021-07-16 13:15:02 -0600

    easy kata that takes a int and returns odd or even

---
+++ evenOdd.js
@@ -0,0 +1,13 @@
+/*
+Create a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+
+function even_or_odd(number) {
+  if (number % 2 == 0) {
+    return "Even";
+  }
+  return "Odd";
+}
+
+const test = even_or_odd(423849);
+console.log(test);
