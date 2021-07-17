a961db3ddacf5f173d2b8d56ea2b77214ceaeac6
nolangh <nolanghiggins@gmail.com>
2021-07-16 18:08:14 -0600

    started a kata that compares one array to another

---
+++ theSame.js
@@ -0,0 +1,17 @@
+
+function comp(array1, array2) {
+  let reducedArray = array2
+    .sort((a, b) => a - b)
+    .forEach((element) => Math.sqrt(element));
+  let sorted1 = array1.sort((a, b) => a - b);
+  if (sorted1 === reducedArray) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const a1 = [3, 4, 0, 0, 6, 1, 1, 1, 9, 5, 6];
+const a2 = [36, 0, 9, 16, 1, 81, 25, 1, 36, 1, 0]
+const test = comp(a1, a2);
+console.log(test);
