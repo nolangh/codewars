953abe52e61c6ea37ecaa6d9264160d5c63a997f
nolangh <nolanghiggins@gmail.com>
2021-07-17 13:15:29 -0600

    trying different methods/ taking a break on it also

--- theSame.js
+++ theSame.js
@@ -1,9 +1,13 @@
-function comp(array1, array2) {
+/*function comp(array1, array2) {
+  //this sorts and reduces array2.The forEach reduces each element in array 2
   let reducedArray = array2
     .sort((a, b) => a - b)
     .forEach((element) => Math.sqrt(element));
-  //let sorted1 = array1.sort((a, b) => a - b);
-}
+  //this sprts array1
+  let sorted1 = array1.sort((a, b) => a - b);
+}*/
+
+const math = require("mathjs");
 
 const a1 = [3, 4, 0, 0, 6, 1, 1, 1, 9, 5, 6];
 const a2 = [36, 0, 9, 16, 1, 81, 25, 1, 36, 1, 0];
@@ -11,5 +15,11 @@ const test = comp(a1, a2);
 console.log(test);
 
 function comp(array1, array2) {
-  array1.forEach(function (element) {});
+  const sorted1 = array1.sort(function (a, b) {
+    return a - b;
+  });
+  const sorted2 = array2.sort(function (a, b) {
+    return a - b;
+  });
+  
 }
