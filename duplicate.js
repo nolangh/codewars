00a0894b3d6c0e0dd2233dd6c7820b49649028ab
nolangh <nolanghiggins@gmail.com>
2021-07-18 14:37:19 -0600

    i have each character counted. i just need to return the number

---
+++ duplicate.js
@@ -0,0 +1,30 @@
+const { i } = require("mathjs");
+
+/*
+Write a function that will return the count of distinct case-insensitive alphabetic characters
+and numeric digits that occur more than once in the input string. The input string can be assumed
+to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+EXAMPLES -----
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+*/
+function duplicateCount(text) {
+  text = text.toLowerCase();
+  let duplicate = {};
+  for (let i = 0; i < text.length; i++) {
+    let character = text.charAt(i);
+    if (duplicate[character]) {
+      duplicate[character]++;
+    } else {
+      duplicate[character] = 1;
+    }
+  }
+  return duplicate;
+}
+
+const test = duplicateCount("aabbcde");
+console.log(test);
