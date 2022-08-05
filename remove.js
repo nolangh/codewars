df0317a5c61bf597879de764399d0996dfc72fd6
nolangh <nolanghiggins@gmail.com>
2022-08-05 12:04:25 -0600

    -a

---
+++ remove.js
@@ -0,0 +1,10 @@
+const firstAndLast = (string) => {
+	//const newString = [];
+	console.log(string);
+	if (string.length <= 2) {
+		return string;
+	}
+	return string;
+	console.log(string);
+};
+console.log(firstAndLast("fi"));
